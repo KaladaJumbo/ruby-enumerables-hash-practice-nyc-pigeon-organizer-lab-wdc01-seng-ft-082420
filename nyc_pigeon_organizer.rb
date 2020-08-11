d5fc842e1a9e11fcb81938aca389a80afd5fa6fc
lively-system-6174 <wkalada1@gmail.com>
2020-08-11 10:28:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -51,8 +51,8 @@ def nyc_pigeon_organizer #(data)
         if data[key][inner_key].detect {|att| att == name}
           
           inner_array.push(inner_key.to_s)
-          binding.pry
-          organized_data[name][key] =  inner_array
+          
+          organized_data[name][key] =  inner_arraybinding.pry
         end
         
       end 
